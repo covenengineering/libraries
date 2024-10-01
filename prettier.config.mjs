a354642ebc86826040fb9d5813c2ad8de697376b
Lou Cyx <git@lou.cx>
2024-10-01 02:04:55 -0500

    🔨 new prettify script.

---
+++ prettier.config.mjs
@@ -0,0 +1,101 @@
+/**
+ * @import { Options } from "npm:prettier";
+ */
+
+/**
+ * Prettier configuration.
+ * @see [Prettier options](https://prettier.io/docs/en/options.html)
+ * @type {Options}
+ */
+export default {
+	/**
+	 * Avoid parentheses around a sole arrow function parameter.
+	 */
+	arrowParens: "avoid",
+
+	/**
+	 * Put the `>` of a multi-line HTML like in a new line.
+	 */
+	bracketSameLine: false,
+
+	/**
+	 * Print spaces between brackets in object literals.
+	 */
+	bracketSpacing: true,
+
+	/**
+	 * Control whether Prettier formats quoted code embedded in the file.
+	 */
+	embeddedLanguageFormatting: "auto",
+
+	/**
+	 * Unix EOL.
+	 */
+	endOfLine: "lf",
+
+	/**
+	 * Use curious ternaries, with the question mark after the condition,
+	 * instead of on the same line as the consequent.
+	 */
+	experimentalTernaries: true,
+
+	/**
+	 * Strict whitespace in HTML.
+	 */
+	htmlWhitespaceSensitivity: "strict",
+
+	/**
+	 * JSX should use double quote.
+	 */
+	jsxSingleQuote: false,
+
+	/**
+	 * Print width is ideal at 80 characters.
+	 */
+	printWidth: 80,
+
+	/**
+	 * Wrap text to fit the print width.
+	 */
+	proseWrap: "always",
+
+	/**
+	 * Add quotes around properties of object if needed.
+	 */
+	quoteProps: "as-needed",
+
+	/**
+	 * Print semicolons at the ends of statements.
+	 */
+	semi: true,
+
+	/**
+	 * Shows multiple attributes per line in HTML, Vue and JSX.
+	 */
+	singleAttributePerLine: false,
+
+	/**
+	 * Use double quotes.
+	 */
+	singleQuote: false,
+
+	/**
+	 * Tab width at 4 is ideal.
+	 */
+	tabWidth: 4,
+
+	/**
+	 * Add trailing commas to make edition easier and diffing better.
+	 */
+	trailingComma: "all",
+
+	/**
+	 * Obviously use tabs.
+	 */
+	useTabs: true,
+
+	/**
+	 * Indent code in Vue files.
+	 */
+	vueIndentScriptAndStyle: true,
+};
--- tests/@coven/terminal/format.test.ts
+++ tests/@coven/terminal/format.test.ts
@@ -6,9 +6,15 @@ Deno.test("Format works on string", () =>
 );
 
 Deno.test("Format works as a template tag function", () =>
-	assertEquals(format(1, 2)`Coven Engineering ${13}`, "[1mCoven Engineering 13[2m"),
+	assertEquals(
+		format(1, 2)`Coven Engineering ${13}`,
+		"[1mCoven Engineering 13[2m",
+	),
 );
 
 Deno.test("Nested format works", () =>
-	assertEquals(format(1, 2)`Hi ${format(3, 2)`witch`}!`, "[1mHi [3mwitch[1m![2m"),
+	assertEquals(
+		format(1, 2)`Hi ${format(3, 2)`witch`}!`,
+		"[1mHi [3mwitch[1m![2m",
+	),
 );
