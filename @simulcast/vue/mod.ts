ba382a5d189c1a705ea953abd81e8e31a65956b1
Lou Cyx <git@lou.cx>
2025-08-08 16:27:12 -0500

    ✅ improve test utils for DOM. ✅ add tests for @simulcast/vue. ✨ add missing mod.ts for @simulcast/vue.

---
+++ @simulcast/vue/mod.ts
@@ -0,0 +1,4 @@
+export { useBroadcast } from "./useBroadcast.ts";
+export type { UseBroadcastObject } from "./UseBroadcastObject.ts";
+export type { UseBroadcastOn } from "./UseBroadcastOn.ts";
+export { useBroadcastProxyHandler } from "./useBroadcastProxyHandler.ts";
