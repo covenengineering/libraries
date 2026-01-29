2c9760cb484fd2b10d6263b6a94ac577805a8293
Lou Cyx <git@lou.cx>
2026-01-29 01:56:41 -0600

    🔥 delete old lint files.

--- lint/rules/no-continue.ts
+++
@@ -1,6 +0,0 @@
-import { no } from "../no.ts";
-
-export const noContinue: Deno.lint.Rule = no(
-	"ContinueStatement",
-	"Avoid using `continue`. Use explicit control flow.",
-);
--- lint/rules/no-function.ts
+++
@@ -1,6 +0,0 @@
-import { no } from "../no.ts";
-
-export const noFunction: Deno.lint.Rule = no(
-	"FunctionDeclaration",
-	"Avoid using `function`. Use an arrow expression instead.",
-);
