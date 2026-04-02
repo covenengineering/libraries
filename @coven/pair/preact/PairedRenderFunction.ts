e396f3f55c57428df24f9d97f670f4076b258c70
Lou Cyx <git@lou.cx>
2026-04-01 20:08:16 -0500

    💩 (@coven/pair) introducing ugly hack of types until `deno doc --lint` is fixed to support types properly.

--- @coven/pair/preact/PairedRenderFunction.ts
+++ @coven/pair/preact/PairedRenderFunction.ts
@@ -1,5 +1,12 @@
 import type { Unary } from "@coven/types";
-import type { Attributes, VNode } from "preact";
+// FIXME: Uncomment once this is fixed: https://github.com/denoland/deno/issues/27364
+// import type { Attributes, VNode } from "preact";
+
+/** @internal */
+type Attributes = import("preact").Attributes;
+
+/** @internal */
+type VNode<P = object> = import("preact").VNode<P>;
 
 /**
  * Function that receives the paired hook and must return a `VNode`.
