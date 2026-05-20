9517b7fe2bddf94fcd1af096cc844ed8b0e5331d
Lou Cyx <git@lou.cx>
2026-05-20 16:41:08 -0500

    ✏️ (shared) Fix some typos here and there.

--- @coven/iterables/async/entriesToObject.ts
+++ @coven/iterables/async/entriesToObject.ts
@@ -21,6 +21,7 @@ import { reduce } from "./reduce.ts";
  * 	["number", 1]
  * ]); // { foo: "bar", number: 1 }
  * ```
+ * @param iterable `AsyncIterable` of entries to turn into an object.
  * @returns Object constructed from entries.
  */
 export const entriesToObject = reduce(
--- @coven/iterables/async/length.ts
+++ @coven/iterables/async/length.ts
@@ -3,15 +3,18 @@ import { always } from "@coven/utils";
 import { reduce } from "./reduce.ts";
 
 /**
- * Get the length of an iterable or asynchronous iterable.
+ * Get the length of an iterable or asynchronous iterable (using `bigint` for
+ * really big iterables).
  *
  * @example
  * ```typescript
- * length([1, 2, 3]); // 3
+ * length([1, 2, 3]); // 3n
  * ```
  * @param iterable Iterable or asynchronous iterable to get the length from.
  * @returns Promise with the length of the iterable.
  */
 export const length: <Iterable extends AwaitableIterable>(
 	iterable: Iterable,
-) => Promise<number> = reduce<unknown, number>(always((total) => total + 1))(0);
+) => Promise<bigint> = reduce<unknown, bigint>(always((total) => total + 1n))(
+	0n,
+);
